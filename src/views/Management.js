d9e8cd42f31525f4de781ad021785ce91075e549
valentinyak <63512456+valentinyak@users.noreply.github.com>
2022-04-23 13:34:04 +0300

    create add functionality

--- src/views/Management.js
+++ src/views/Management.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import ClipLoader from 'react-spinners/ClipLoader';
 import { getBanks, addBankToDB, deleteBankFromDB } from '../services/banks-api';
 
@@ -13,6 +13,26 @@ function Management() {
 
     setLoading(true);
     deleteBankFromDB(bank).then(() => setRenderCount(0));
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const { bankName, maxLoan, minDownPayment, loanTerm } =
+      e.currentTarget.form;
+    const maxLoanInt = parseInt(maxLoan.value);
+    const minDownPaymentInt = parseInt(minDownPayment.value);
+    const loanTermInt = parseInt(loanTerm.value);
+
+    const newBank = {
+      name: bankName.value,
+      max_loan: maxLoanInt,
+      min_down_payment: minDownPaymentInt,
+      loan_term: loanTermInt,
+    };
+
+    setLoading(true);
+    addBankToDB(newBank).then(() => setRenderCount(0));
   };
 
   useEffect(() => {
@@ -68,12 +88,57 @@ function Management() {
                   <button type="button" onClick={handleDelete} id={bank._id}>
                     delete
                   </button>
+                  <button type="button" onClick={handleDelete} id={bank._id}>
+                    edit
+                  </button>
                 </tr>
               );
             })}
           </tbody>
         </table>
       )}
+
+      <form
+        action=""
+        style={{ display: 'flex', flexDirection: 'column', width: '350px' }}
+      >
+        <h4>Add bank</h4>
+        <label
+          htmlFor=""
+          style={{ display: 'flex', justifyContent: 'space-between' }}
+        >
+          {' '}
+          Bank name
+          <input type="text" name="bankName" />
+        </label>
+        <label
+          htmlFor=""
+          style={{ display: 'flex', justifyContent: 'space-between' }}
+        >
+          {' '}
+          Max loan
+          <input type="text" name="maxLoan" />
+        </label>
+        <label
+          htmlFor=""
+          style={{ display: 'flex', justifyContent: 'space-between' }}
+        >
+          {' '}
+          Min down payment
+          <input type="text" name="minDownPayment" />
+        </label>
+        <label
+          htmlFor=""
+          style={{ display: 'flex', justifyContent: 'space-between' }}
+        >
+          {' '}
+          Loan term
+          <input type="text" name="loanTerm" />
+        </label>
+        <button type="button" onClick={handleSubmit}>
+          Add new bank
+        </button>
+      </form>
     </div>
   );
 }
